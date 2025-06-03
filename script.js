6b4d85d3e5d6a7ebb2854fba89a32e8effe8c1dd
nithig <154483033+nithig@users.noreply.github.com>
2025-06-03 17:11:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,14 @@ const products = [
   { id: 4, name: "Product 4", price: 40 },
   { id: 5, name: "Product 5", price: 50 },
 ];
+sessionStorage.setItem("cart", JSON.stringify([
+  { id: 1, name: "Product 1", price: 10 },
+  { id: 5, name: "Product 5", price: 50 },
+  { id: 1, name: "Product 1", price: 10 }
+]));
 
+renderProducts();
+renderCart();
 // DOM elements
 const productList = document.getElementById("product-list");
 const cartList = document.getElementById("cart-list");
